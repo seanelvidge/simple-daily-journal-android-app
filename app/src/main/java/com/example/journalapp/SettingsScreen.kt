0cf707ba05a8568f82913d59f059b9269db70119
Sean Elvidge <s.elvidge@bham.ac.uk>
2026-01-16 23:32:13 +0000

    Fix missing import in Settings screen

--- app/src/main/java/com/example/journalapp/SettingsScreen.kt
+++ app/src/main/java/com/example/journalapp/SettingsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
