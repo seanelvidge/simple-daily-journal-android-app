8be7bd6921cea3620cea9d3d2fd5f66e502f4d98
Sean Elvidge <s.elvidge@bham.ac.uk>
2026-01-17 22:33:03 +0000

    Fix missing TextButton import

--- app/src/main/java/com/example/journalapp/JournalScreen.kt
+++ app/src/main/java/com/example/journalapp/JournalScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
